a88aa2cd992c7ea3a15cc73460840b8a0ebfd1cc
Risal Hidayat <risalhidayat88@gmail.com>
2025-09-15 17:00:07 +0700

    Add sleep management tests and fixtures
    - Implemented tests for the SleepsController, covering index, create, update, and error scenarios for user sleep sessions.
    - Added fixture data for sleeps to support testing.
    - Updated FollowsControllerTest to assert correct follow relationships.
    - Created a new test file for Sleep model validations.

---
+++ test/controllers/api/v1/me/sleeps_controller_test.rb
@@ -0,0 +1,56 @@
+class Api::V1::Me::SleepsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get api_v1_me_sleeps_url, headers: { "X-User-Id" => users(:one).id }
+
+    assert_response :success
+
+    json_response = JSON.parse(response.body)
+
+    assert_equal json_response.length, 1
+    assert_equal json_response[0]["duration"], sleeps(:one).duration
+  end
+
+  test "should create sleep" do
+    post api_v1_me_sleeps_url, headers: { "X-User-Id" => users(:one).id }
+
+    assert_response :created
+
+    json_response = JSON.parse(response.body)
+
+    assert_not_nil json_response["sleep_start"]
+    assert_nil json_response["duration"]
+
+    assert_equal Sleep.count, 3
+  end
+
+  test "should not create sleep if user already has an active sleep session" do
+    sleep = sleeps(:active)
+    post api_v1_me_sleeps_url, headers: { "X-User-Id" => sleep.user_id }
+
+    assert_response :unprocessable_content
+  end
+  
+  test "should update sleep" do
+    sleep = sleeps(:active)
+    patch api_v1_me_sleep_url(id: sleep.id), headers: { "X-User-Id" => sleep.user_id }
+
+    assert_response :ok
+
+    json_response = JSON.parse(response.body)
+
+    assert_not_nil json_response["duration"]
+    assert_not_nil json_response["sleep_end"]
+  end
+
+  test "should not update sleep if sleep already ended" do
+    patch api_v1_me_sleep_url(id: sleeps(:one).id), headers: { "X-User-Id" => users(:one).id }
+
+    assert_response :unprocessable_content
+  end
+
+  test "should not update sleep if sleep is not found" do
+    patch api_v1_me_sleep_url(id: 999), headers: { "X-User-Id" => users(:one).id }
+
+    assert_response :not_found
+  end
+end
