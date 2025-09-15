c0a7da5ab545cf4985407a1f045776dc5ee7c826
Risal Hidayat <risal.hidayat@kawanlamacorp.com>
2025-09-15 13:24:16 +0700

    add several methods in application controller to authenticate user by X-User-Id header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_user!
+
+  def current_user
+    @current_user ||= User.find_by(id: header_user_id)
+  end
+
+  private
+
+  def authenticate_user!
+    raise "Unauthorized" if header_user_id.blank?
+    raise "Unauthorized" if current_user.blank?
+  end
+
+  def header_user_id
+    request.headers["X-User-Id"]
+  end
 end
