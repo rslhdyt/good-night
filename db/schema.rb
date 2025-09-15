6060a81a368977e3c823aa828f73179464f68470
Risal Hidayat <risalhidayat88@gmail.com>
2025-09-15 15:24:29 +0700

    Add follows table and migration for user relationships
    - Created follows table with references to follower and followed users.
    - Added foreign keys and unique index to ensure data integrity.
    - Updated schema version to reflect the new changes.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_15_062103) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_15_072135) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "follows", force: :cascade do |t|
+    t.bigint "follower_id", null: false
+    t.bigint "followed_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_follows_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_follows_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_follows_on_follower_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "follows", "users", column: "followed_id"
+  add_foreign_key "follows", "users", column: "follower_id"
 end
