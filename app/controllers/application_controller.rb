7e4079302dc535d4257628f534747367ca2f01d4
Risal Hidayat <risal.hidayat@kawanlamacorp.com>
2025-09-15 14:09:23 +0700

    Add custom Unauthorized error handling in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::API
   before_action :authenticate_user!
 
+  class Unauthorized < StandardError; end
+
   def current_user
     @current_user ||= User.find_by(id: header_user_id)
   end
@@ -8,11 +10,15 @@ class ApplicationController < ActionController::API
   private
 
   def authenticate_user!
-    raise "Unauthorized" if header_user_id.blank?
-    raise "Unauthorized" if current_user.blank?
+    raise Unauthorized if header_user_id.blank?
+    raise Unauthorized if current_user.blank?
   end
 
   def header_user_id
     request.headers["X-User-Id"]
+  end
+
+  rescue_from Unauthorized do
+    render json: { error: "unauthorized" }, status: :unauthorized
   end
 end
