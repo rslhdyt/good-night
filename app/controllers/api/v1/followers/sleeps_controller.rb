27b17733f25524212073f9e658e6e87445816818
Risal Hidayat <risalhidayat88@gmail.com>
2025-09-15 16:59:22 +0700

    Add SleepsController for retrieving user sleep data
    - Implemented index action to fetch sleeps for a specific follower, including user details.
    - Added authentication requirement for accessing the action.
    - Limited results to the previous week and sorted by duration.

---
+++ app/controllers/api/v1/followers/sleeps_controller.rb
@@ -0,0 +1,20 @@
+module Api
+  module V1
+    module Followers
+      class SleepsController < ApplicationController
+        before_action :authenticate_user!
+
+        def index
+            sleeps = Sleep.joins("INNER JOIN follows ON follows.followed_id = sleeps.user_id")
+              .where(follows: { follower_id: params[:follower_id] })
+              .includes(:user)
+              .previous_week
+              .sorted_by_duration
+              .limit(10)
+
+            render json: sleeps, include: { user: { only: :name } }
+        end
+      end
+    end
+  end
+end
